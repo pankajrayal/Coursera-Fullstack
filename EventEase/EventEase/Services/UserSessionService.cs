4559b3524455319035148bd2b0ec8c6418e2696b
Pankaj Rayal <pankajrayal@gmail.com>
2025-04-18 10:54:36 -0500

    updated eventbase project

---
+++ EventEase/EventEase/Services/UserSessionService.cs
@@ -0,0 +1,12 @@
+﻿public class UserSessionService {
+  public string? Username { get; private set; }
+  public bool IsLoggedIn => !string.IsNullOrEmpty(Username);
+
+  public void Login(string username) {
+    Username = username;
+  }
+
+  public void Logout() {
+    Username = null;
+  }
+}
