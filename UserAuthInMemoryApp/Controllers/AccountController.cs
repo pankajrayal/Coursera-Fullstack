d74e972f403cb6d8619773348c2b71598d93a72f
Pankaj Rayal <pankajrayal@gmail.com>
2025-04-21 18:11:22 -0500

    Login Register Demo

---
+++ UserAuthInMemoryApp/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserAuthInMemoryApp.Models;
+
+namespace UserAuthInMemoryApp.Controllers {
+  public class AccountController: Controller {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) {
+      _userManager = userManager;
+      _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login() {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model) {
+      if(ModelState.IsValid) {
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+        if(result.Succeeded) {
+          return RedirectToAction("Index", "Home");
+        }
+        ModelState.AddModelError("", "Invalid login attempt.");
+      }
+      return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Register() {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model) {
+      if(ModelState.IsValid) {
+        var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if(result.Succeeded) {
+          return RedirectToAction("Login");
+        }
+        foreach(var error in result.Errors) {
+          ModelState.AddModelError("", error.Description);
+        }
+      }
+      return View(model);
+    }
+  }
+}
