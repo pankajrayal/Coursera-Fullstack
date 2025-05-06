cc6ee1ab2309bd7017222e11c6dbbbb58fa5ca59
Pankaj Rayal <pankajrayal@gmail.com>
2025-05-06 11:59:43 -0500

    Step 3: Create Services in Blazor

---
+++ SkillSnap/SkillSnap.Client/Services/ProjectService.cs
@@ -0,0 +1,20 @@
+﻿using SkillSnap.Client.Models;
+using System.Net.Http.Json;
+
+namespace SkillSnap.Client.Services {
+  public class ProjectService {
+    private readonly HttpClient _httpClient;
+
+    public ProjectService(HttpClient httpClient) {
+      _httpClient = httpClient;
+    }
+
+    public async Task<List<Project>> GetProjectsAsync() {
+      return await _httpClient.GetFromJsonAsync<List<Project>>("api/projects");
+    }
+
+    public async Task AddProjectAsync(Project newProject) {
+      await _httpClient.PostAsJsonAsync("api/projects", newProject);
+    }
+  }
+}
---
+++ SkillSnap/SkillSnap.Client/Services/SkillService.cs
@@ -0,0 +1,20 @@
+﻿using SkillSnap.Client.Models;
+using System.Net.Http.Json;
+
+namespace SkillSnap.Client.Services {
+  public class SkillService {
+    private readonly HttpClient _httpClient;
+
+    public SkillService(HttpClient httpClient) {
+      _httpClient = httpClient;
+    }
+    public async Task<List<Skill>> GetSkillsAsync() {
+      return await _httpClient.GetFromJsonAsync<List<Skill>>("api/skills");
+    }
+
+    public async Task AddSkillAsync(Skill newSkill) {
+      await _httpClient.PostAsJsonAsync("api/skills", newSkill);
+    }
+
+  }
+}
