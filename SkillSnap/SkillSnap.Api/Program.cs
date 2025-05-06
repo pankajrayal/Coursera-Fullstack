7acc92e3609f9912f7e00353d150b32f5550d559
Pankaj Rayal <pankajrayal@gmail.com>
2025-05-06 11:17:01 -0500

    Step 4: Populate the Database with Sample Data

--- SkillSnap/SkillSnap.Api/Program.cs
+++ SkillSnap/SkillSnap.Api/Program.cs
@@ -6,13 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
 // Configure MSSQL Server
 builder.Services.AddDbContext<SkillSnapContext>(options => {
   options.UseSqlServer(builder.Configuration.GetConnectionString("SkillSnapDb"));
 });
 
 var app = builder.Build();
-
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment()) {
